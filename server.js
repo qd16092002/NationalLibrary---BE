6ffa6ee71f73f3092e3d066bb2c7fd587d8392ea
QuangDao <tranquangdao16092002@gmail.com>
2023-02-17 14:27:30 +0700

    code BE list user

--- server.js
+++ server.js
@@ -122,6 +122,17 @@ app.post("/register", async (req, res) => {
     res.status(500).send("Lỗi server");
   }
 });
+
+app.get("/user", async (req, res) => {
+  try {
+    const users = await User.find();
+
+    res.status(200).send(users);
+  } catch (error) {
+    console.log("error:", error);
+    res.status(500).send("Lỗi server");
+  }
+});
 // Đăng nhập
 app.post("/login", async (req, res) => {
   try {
